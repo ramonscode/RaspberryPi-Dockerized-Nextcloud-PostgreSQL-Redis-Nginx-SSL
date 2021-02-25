3578693f989e06dd950fffc55215e4984595ea43
Ramón Aguilera <donragui@tutanota.com>
2021-02-26 00:37:38 +0100

    Add readability

--- build.sh
+++ build.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 # Parameters
 domain=(mydomain.com)
+email="email@mydomain.com" # Adding a valid address is strongly recommended
 rsa_key_size=4096
 data_path="./data/certbot"
-email="email@mydomain.com" # Adding a valid address is strongly recommended
 staging=0 # Set to 1 if you're testing your setup to avoid hitting request limits
 
 # Check docker-compose
